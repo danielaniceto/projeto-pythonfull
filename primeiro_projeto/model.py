df4b1d8f27c5ec2e9b96f694fb372e5df636ab1a
danielaniceto <danielgustavots@hotmail.com>
2024-02-02 16:57:57 -0300

    Adião da classe produto

--- primeiro_projeto/model.py
+++ primeiro_projeto/model.py
@@ -1,3 +1,12 @@
 class Categoria():
     def __init__(self, categoria):
         self.categoria = categoria
+
+class Produtos():
+    def __init__(self, produto, nome, preco, id, peso):
+        self.produto = produto
+        self.nome = nome
+        self.preco = preco
+        self.id = id
+        self.peso = peso
+        
