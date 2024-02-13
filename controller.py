ddc19bfeb18486d4be0814261628497ae8ba5b15
danielaniceto <danielgustavots@hotmail.com>
2024-02-13 17:12:58 -0300

    Codigos class alterarCategoria + fornecedores.txt

--- controller.py
+++ controller.py
@@ -35,3 +35,29 @@ class CategoriaController:
                 for i in x:
                     arqcategorias.writelines(i.categorias)
                     arqcategorias.writelines("\n")
+
+    def alterarCategoria(self, alterar_categoria, categoria_alterada):
+        x = CategoriaDao.ler_categoria()
+
+        list_categoria = list(filter(lambda x: x.categoria == alterar_categoria, x))
+
+        if len(list_categoria) > 0:
+            list_categorias = list(filter(lambda x: x.categoria == categoria_alterada, x))
+
+            if len(list_categorias) == 0:
+                x = list(map(lambda x: Categoria(categoria_alterada) if(x.categoria == alterar_categoria) else(x), x))
+                print("Alteração realizada com sucesso!!!")
+
+            else:
+                print("Categoria ja existente!!!")
+
+        else:
+            print("Categoria inexistente para alteração")
+
+        with open("Categorias.txt", "w") as arqcategorias:
+                for i in x:
+                    arqcategorias.writelines(i.categoria)
+                    arqcategorias.writelines("\n")
+
+#a = CategoriaController()
+#a.alterarCategoria("Frios", "Frios")
