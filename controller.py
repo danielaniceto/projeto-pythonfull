f8da3e8117bcb3941a97ec961bd8d5824009dc29
danielaniceto <danielgustavots@hotmail.com>
2024-02-13 14:04:28 -0300

    Codificação da criação de categoria e remoção, + arquivos txts

--- controller.py
+++ controller.py
@@ -11,10 +11,27 @@ class CategoriaController:
         
         if not existe_categoria:
             CategoriaDao.salvar_categoria(novacategoria)
-            return "Categoria salva com sucesso!!!"
+            return ("Categoria salva com sucesso!!!")
         else:
-            return "A categoria ja existe!!!"
+            return ("A categoria ja existe!!!")
         
-x = CategoriaController()
-x.cadastrarNovaCategoria("Frios")
+#x = CategoriaController()
+#x.cadastrarNovaCategoria("Frutas")
 
+    def removerCategoria(self, remover_categoria):
+        x = CategoriaDao.ler_categoria()
+        list_categoria = list(filter(lambda x: x.categorias == remover_categoria, x))
+        
+        if len(list_categoria) <= 0:
+                return "A categoria que pretende exlcuir não existe!!!"
+        else:
+            for i in range(len(x)):
+                if x[i].categorias == remover_categoria:
+                    del x[i]
+                    break
+            print ("Categoria removida com sucesso!!!")
+
+            with open("Categorias.txt", "w") as arqcategorias:
+                for i in x:
+                    arqcategorias.writelines(i.categorias)
+                    arqcategorias.writelines("\n")
